85979e86669110272f6ad0a157c9c725c310ff02
mabq <alejandro.banderas@me.com>
2019-12-26 10:04:20 -0500

    Some changes before starting with promises

--- exercises/58 - Gallery/gallery-prototype-FINISHED.js
+++ exercises/58 - Gallery/gallery-prototype-FINISHED.js
@@ -2,9 +2,10 @@ function Gallery(gallery) {
   if (!gallery) {
     throw new Error('No Gallery Found!');
   }
+
   this.gallery = gallery;
+
   // select the elements we need
-  this.images = Array.from(gallery.querySelectorAll('img'));
   this.modal = document.querySelector('.modal');
   this.prevButton = this.modal.querySelector('.prev');
   this.nextButton = this.modal.querySelector('.next');
@@ -16,17 +17,11 @@ function Gallery(gallery) {
   this.handleClickOutside = this.handleClickOutside.bind(this);
 
   // These are our Event Listeners!
-  this.images.forEach(image =>
-    image.addEventListener('click', e => this.showImage(e.currentTarget))
-  );
-
-  // loop over each image
-  this.images.forEach(image => {
-    // attach an event listener for each image
+  const images = Array.from(gallery.querySelectorAll('img'));
+  images.forEach(image => {
+    image.addEventListener('click', e => this.showImage(e.currentTarget));
     image.addEventListener('keyup', e => {
-      // when that is keyup'd, check if it was enter
       if (e.key === 'Enter') {
-        // if it was, show that image
         this.showImage(e.currentTarget);
       }
     });
@@ -35,12 +30,24 @@ function Gallery(gallery) {
   this.modal.addEventListener('click', this.handleClickOutside);
 }
 
+Gallery.prototype.showImage = function(el) {
+  if (!(el instanceof Element) || !el.src) {
+    console.info('No image to show.');
+    return;
+  }
+  // update the modal with this info
+  this.modal.querySelector('img').src = el.src;
+  this.modal.querySelector('h2').textContent = el.title;
+  this.modal.querySelector('figure p').textContent = el.dataset.description;
+  this.currentImage = el;
+  this.openModal();
+};
+
 Gallery.prototype.openModal = function() {
   console.info('Opening Modal...');
-  // First check if the modal is already open
   if (this.modal.matches('.open')) {
-    console.info('Madal already open');
-    return; // stop the function from running
+    console.info('Modal is already open');
+    return;
   }
   this.modal.classList.add('open');
 
@@ -76,29 +83,13 @@ Gallery.prototype.showNextImage = function() {
     this.currentImage.nextElementSibling || this.gallery.firstElementChild
   );
 };
+
 Gallery.prototype.showPrevImage = function() {
   this.showImage(
     this.currentImage.previousElementSibling || this.gallery.lastElementChild
   );
 };
 
-Gallery.prototype.showImage = function(el) {
-  if (!el) {
-    console.info('no image to show');
-    return;
-  }
-  // update the modal with this info
-  console.log(el);
-  this.modal.querySelector('img').src = el.src;
-  this.modal.querySelector('h2').textContent = el.title;
-  this.modal.querySelector('figure p').textContent = el.dataset.description;
-  this.currentImage = el;
-  this.openModal();
-};
-
 // Use it on the page
-
 const gallery1 = new Gallery(document.querySelector('.gallery1'));
 const gallery2 = new Gallery(document.querySelector('.gallery2'));
-
-console.log(gallery1, gallery2);
